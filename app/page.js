e881bf8b3c2cf045b2201ddc40fe475195067311
berkesasa <berkesasa11@gmail.com>
2023-10-23 21:35:20 +0300

    adding meta title

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Welcome from './components/Welcome'
 
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Berke SASA Portfolio',
+  description: "Let's look my portfolio :)",
+}
 
 function Page() {
     return (
