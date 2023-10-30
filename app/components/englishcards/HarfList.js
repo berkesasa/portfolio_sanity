29ff31d9bea746c674a99012ada898bada9af717
berkesasa <berkesasa11@gmail.com>
2023-10-30 18:12:01 +0300

    updated cards

--- app/components/englishcards/HarfList.js
+++ app/components/englishcards/HarfList.js
@@ -6,7 +6,7 @@ export default function HarfList({ text }) {
     const letters = text.split(""); // Metni harf harf ayırın
 
     return (
-        <div className="flex space-x-2 absolute -top-20 flex-wrap">
+        <div className="flex space-x-2 absolute -top-20 max-sm:flex-wrap">
             {letters.map((letter, index) => (
                 <div key={index} className="shadow-lg flex items-center justify-center rounded-full w-8 h-8 text-sm bg-[#176B87] text-white">
                     {letter}
